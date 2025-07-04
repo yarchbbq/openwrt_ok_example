5c98f4277c8272912d637f3532e1e3eae45cc8fc
yarchbbq <67890508+yarchbbq@users.noreply.github.com>
2025-07-04 09:57:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git cups https://github.com/Gr4ffy/lede-cups.git" >> feeds.conf.default
+#echo "src-git cups https://github.com/Gr4ffy/lede-cups.git" >> feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo "src-git parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git" >> feeds.conf.default
